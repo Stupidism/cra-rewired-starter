0ba641188fb630b188af58dc53d02f57823a13bc
stupid <sunfeng32@qq.com>
2017-11-23 16:27:13 +0800

    feat(react-router): add NotFound page

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import store from './store';
+import NotFound from './routes/NotFound';
 
 import './index.css';
 import App from './App';
@@ -10,7 +12,12 @@ import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route component={NotFound} />
+      </Switch>
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root'),
 );
---
+++ src/routes/NotFound.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default () => (
+  <h1>
+    Not found. Go to <Link to="/">Home</Link>
+  </h1>
+);
