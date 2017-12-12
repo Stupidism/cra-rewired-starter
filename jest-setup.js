b9262ae9f2f4bdfaab27fd3acc87e1f4bd3b7d52
stupid <sunfeng32@qq.com>
2017-12-12 17:46:27 +0800

    chore(test): support shallow.until

--- jest-setup.js
+++ jest-setup.js
@@ -1,6 +1,11 @@
 import {createSerializer} from 'enzyme-to-json';
+
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import ShallowWrapper from 'enzyme/ShallowWrapper';
+import until from 'react-render-counter/utils/testHelpers/until';
+
+expect.addSnapshotSerializer(createSerializer({mode: 'deep'}));
 
 Enzyme.configure({ adapter: new Adapter() });
-expect.addSnapshotSerializer(createSerializer({mode: 'deep'}));
+ShallowWrapper.prototype.until = until;
