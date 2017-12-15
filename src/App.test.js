74b9d1198076581250cc3ccdee8e69d9e8a4933a
stupid <sunfeng32@qq.com>
2017-12-15 13:46:46 +0800

    test(App): snapshot it

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
+import menuItems from 'constants/menuItems';
+
 import { App } from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App selectedMenuItem={{}} />, div);
+describe('<App />', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App selectedMenuItem={menuItems[0]} />, div);
+  });
+
+  it('keeps unchanged', () => {
+    const tree = renderer
+      .create(<App selectedMenuItem={menuItems[1]} />)
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
 });
