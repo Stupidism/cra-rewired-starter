a7931799a14520ec4ffee696d69e16b2ff64179e
Stupidism <5801015+Stupidism@users.noreply.github.com>
2018-05-03 17:40:32 +0800

    chore(jest): config threshold

--- config/override-jest.js
+++ config/override-jest.js
@@ -1,4 +1,13 @@
 module.exports = function overrideJest(config) {
   config.setupTestFrameworkScriptFile = '<rootDir>/config/jest-setup.js';
+  config.coverageThreshold = {
+    global: {
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90,
+    },
+  };
+
   return config;
 };
