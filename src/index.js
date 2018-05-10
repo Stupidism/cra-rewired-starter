7666a3607a4af2288ede8a43c5a0247d5ac80a6f
Stupidism <5801015+Stupidism@users.noreply.github.com>
2018-05-10 13:53:43 +0800

    fix(entry): use hashrouter for gh-pages

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import store from './store';
 import './index.css';
@@ -10,9 +10,9 @@ import registerServiceWorker from './utils/registerServiceWorker';
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>,
   document.getElementById('root'),
 );
