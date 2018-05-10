339716498a73a82c4d44f46f99ce30501ebb7997
Stupidism <5801015+Stupidism@users.noreply.github.com>
2018-05-10 11:19:33 +0800

    test(connect): test branches

--- src/store/connect.js
+++ src/store/connect.js
@@ -4,19 +4,19 @@ import { wrapMapToPropsFunc } from 'react-redux/lib/connect/wrapMapToProps';
 
 import _ from 'lodash';
 
+export const createMapStateToPropsFromStateMapper = stateMapper => state =>
+  _.mapValues(stateMapper, selector => {
+    if (typeof selector === 'function') {
+      return selector(state);
+    }
+
+    throw new Error(`Invalid selector type ${typeof selector} for stateMapper`);
+  });
+
 export const whenMapStateToPropsIsObject = stateMapper => {
   if (typeof stateMapper !== 'object') return undefined;
 
-  const mapStateToProps = state =>
-    _.mapValues(stateMapper, selector => {
-      if (typeof selector === 'function') {
-        return selector(state);
-      }
-
-      throw new Error(
-        `Invalid selector type ${typeof selector} for stateMapper`,
-      );
-    });
+  const mapStateToProps = createMapStateToPropsFromStateMapper(stateMapper);
 
   return wrapMapToPropsFunc(mapStateToProps, 'mapStateToProps');
 };
--- src/store/tests/connect.test.js
+++ src/store/tests/connect.test.js
@@ -2,31 +2,56 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import { configureStore } from '../index';
-import connect from '../connect';
+import connect, {
+  createMapStateToPropsFromStateMapper,
+  whenMapStateToPropsIsObject,
+} from '../connect';
 
-test('[store/connect] should accept an object of selectors mapped by name as mapStateToProps', () => {
-  const selectFoo = jest.fn(state => state.foo);
-  const selectBar = jest.fn(state => state.bar);
+describe('[store] connect(mapStateToProps: object | function): HOC', () => {
+  it('should accept an object of selectors mapped by name as mapStateToProps', () => {
+    const stateMock = {
+      foo: 1,
+      bar: jest.mock(),
+    };
+    const stateMapper = {
+      foo: jest.fn(state => state.foo),
+      bar: jest.fn(state => state.bar),
+    };
 
-  const ConnectedComponent = connect({
-    foo: selectFoo,
-    bar: selectBar,
-  })(() => <div />);
+    const ConnectedComponent = connect(stateMapper)(() => <div />);
 
-  const stateMock = {
-    foo: 1,
-    bar: jest.mock(),
-  };
+    const storeMock = configureStore(stateMock);
 
-  const storeMock = configureStore(stateMock);
+    const wrapper = shallow(<ConnectedComponent store={storeMock} />);
 
-  const wrapper = shallow(<ConnectedComponent store={storeMock} />);
+    expect(stateMapper.foo).toHaveBeenCalledTimes(1);
+    expect(stateMapper.foo).toBeCalledWith(stateMock);
+    expect(stateMapper.bar).toHaveBeenCalledTimes(1);
+    expect(stateMapper.bar).toBeCalledWith(stateMock);
 
-  expect(selectFoo).toHaveBeenCalledTimes(1);
-  expect(selectFoo).toBeCalledWith(stateMock);
-  expect(selectBar).toHaveBeenCalledTimes(1);
-  expect(selectBar).toBeCalledWith(stateMock);
+    expect(wrapper.prop('foo')).toBe(stateMock.foo);
+    expect(wrapper.prop('bar')).toBe(stateMock.bar);
+  });
+});
 
-  expect(wrapper.prop('foo')).toBe(stateMock.foo);
-  expect(wrapper.prop('bar')).toBe(stateMock.bar);
+describe('[store] whenMapStateToPropsIsObject: factory', () => {
+  it('should return undefined to let other factory works when stateMapper is not object', () => {
+    expect(whenMapStateToPropsIsObject(jest.fn())).toBeUndefined();
+  });
+});
+
+describe('[store] createMapStateToPropsFromStateMapper(stateMapper: object<selector>): mapStateToProps: state => object<selector(state)>', () => {
+  it('should throw `Invalid selector type` error when selector is not function', () => {
+    expect(
+      createMapStateToPropsFromStateMapper({
+        foo: {},
+      }),
+    ).toThrow('Invalid selector type object for stateMapper');
+
+    expect(
+      createMapStateToPropsFromStateMapper({
+        foo: 1,
+      }),
+    ).toThrow('Invalid selector type number for stateMapper');
+  });
 });
