a1558011b060f451198a7d8c55b43a79485187f0
Stupidism <5801015+Stupidism@users.noreply.github.com>
2018-05-17 20:36:38 +0800

    fix(style): fix layout

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Layout } from 'antd';
 
 import connect from 'store/connect';
 import logoSpinning from 'store/selectors/logoSpinning';
@@ -8,14 +9,14 @@ import logo from 'assets/logo.svg';
 import './Header.less';
 
 export const HeaderRenderer = ({ logoSpinning }) => (
-  <header className="Header">
+  <Layout.Header className="Header">
     <img
       src={logo}
       className={`Logo ${logoSpinning ? '' : 'LogoSpinPaused'}`}
       alt="logo"
     />
     <h1 className="Title">Welcome to React</h1>
-  </header>
+  </Layout.Header>
 );
 
 export default connect({ logoSpinning })(HeaderRenderer);
