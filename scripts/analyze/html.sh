43c4342b993844af357532e4c9f23e35debe6357
Stupidism <5801015+Stupidism@users.noreply.github.com>
2018-05-25 02:49:30 +0800

    fix(scripts.analyze.html): fix ( bug for travis

--- scripts/analyze/html.sh
+++ scripts/analyze/html.sh
@@ -4,7 +4,9 @@ if [ ! -d analysis ]; then
   mkdir analysis
 fi
 
-if [[ $1 =~ .*/(.*)\.[0-9a-z]{8}\.((chunk\.)?js) ]]; then
+re='.*/(.*)\.[0-9a-z]{8}\.((chunk\.)?js)';
+
+if [[ $1 =~ $re ]]; then
   dist=analysis/${BASH_REMATCH[1]}.${BASH_REMATCH[2]}.html;
   source-map-explorer --html $1 > $dist;
   echo Dump into $dist;
