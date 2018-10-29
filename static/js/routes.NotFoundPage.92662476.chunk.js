11cfca363784046070fb9920434ceeee53186c3c
Travis CI User <travis@example.org>
2018-10-29 07:27:39 +0000

    Updates

--- static/js/routes.NotFoundPage.92662476.chunk.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([1],{387:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var o=n(0),a=n.n(o),u=n(83),r=function(){return a.a.createElement("h1",null,"Page Not found. Go ",a.a.createElement(u.b,{to:"/"},"Home"))};t.default=r}});
-//# sourceMappingURL=routes.NotFoundPage.92662476.chunk.js.map
